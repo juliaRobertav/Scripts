fe5d907514266b8d95c19a593d6a3f3dacccb239
julinha <114812014+juliaRobertav@users.noreply.github.com>
2023-07-24 14:13:54 -0300

    Update exercicio1.ps1

--- Lições/exercicio1.ps1
+++ Lições/exercicio1.ps1
@@ -1,6 +1,5 @@
+#converte a data digitada pelo usuáro em um objeto DateTime
+$dataInicial = [DateTime]::ParseExact((Read-Host "Digite a data (formato: dd/mm/yyyy)"), "dd/MM/yyyy", $null)
 
-# Solicita ao usuário que insira uma data e converte em um objeto DateTime
-$startDate = [DateTime]::ParseExact((Read-Host "Digite a data (formato: dd/mm/yyyy)"), "dd/MM/yyyy", $null)
-
-# Calcula a diferença entre as duas datas (em dias) e exibe o resultado
-"Dias contados desde $startDate até hoje: $((Get-Date) - $startDate).Days dias."
+#calcula a diferença entre as duas datas (em dias)
+"Dias contados desde $dataInicial até hoje: $((Get-Date) - $dataInicial) dias."
