4fb134324d274120f96217f082378f7c15007f1d
julinha <114812014+juliaRobertav@users.noreply.github.com>
2023-07-24 09:21:38 -0300

    Update exemplo3.ps1

--- exemplo3.ps1
+++ exemplo3.ps1
@@ -1,4 +1,4 @@
 #script que recebe dados do usuário e armazena em uma variável
 $inicio = "Olá"
 $nome = Read-Host "Qual seu nome?"
-"$inicio, $nome"
+"$inicio, $nome" #outro modo de mostrar o conteudo da variavel
