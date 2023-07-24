a11301eaef18a97f4be5182ea710f0e9f0cbc035
julinha <114812014+juliaRobertav@users.noreply.github.com>
2023-07-24 16:07:29 -0300

    Update exercicio2.ps1

--- Lições/exercicio2.ps1
+++ Lições/exercicio2.ps1
@@ -1,3 +1,3 @@
 #lista
-$lista = "maçã", "laranja", "melancia", "abacaxi" | Get-Random
-"$lista"
+$lista = "maçã", "laranja", "melancia", "abacaxi", "mamão", "abacate", "uva" | Get-Random
+"A fruta escolhida foi:$lista"
