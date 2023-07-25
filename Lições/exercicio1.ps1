7b5982a4271881951f114e3709df20dffeb8149f
julinha <114812014+juliaRobertav@users.noreply.github.com>
2023-07-25 11:02:42 -0300

    Update exercicio1.ps1

--- Lições/exercicio1.ps1
+++ Lições/exercicio1.ps1
@@ -1,4 +1,4 @@
 $dataAtual = Get-Date -DisplayHint Date
 $dataUsuario =  [DateTime]::ParseExact((Read-Host "Digite a Data"), "dd/MM/yyyy", $null)#converte em data e hora
-$calculo = $dataAtual - $dataUsuario
-"Dias e horas calculados desde $dataUsuario até hoje:$calculo"
+$calculo = $dataAtual - $dataUsuario 
+$calculo.ToString("dd") 
