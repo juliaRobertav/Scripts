89ee9b02475bd2ac598ae075c16599b466a3b20f
julinha <114812014+juliaRobertav@users.noreply.github.com>
2023-07-25 10:02:46 -0300

    Update TelaFrutas.ps1

--- Lições/TelaFrutas.ps1
+++ Lições/TelaFrutas.ps1
@@ -1,18 +1,46 @@
-Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Windows.Forms #classe da tela
 Add-Type -AssemblyName System.Drawing
 
+#tela principal
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Frutas"
-$form.Size = New-Object System.Drawing.Size(300,200)
+$form.Size = New-Object System.Drawing.Size(500,600)
 $form.StartPosition = 'CenterScreen'
+# cor de fundo
+$backColor = [System.Drawing.Color]::FromArgb(240, 216, 211)
+$form.BackColor = $backColor
 
-$Label = New-Object System.Windows.Forms.Label
-#lista
-$lista = "maçã", "laranja", "melancia", "abacaxi", "mamão", "abacate", "uva" | Get-Random
-$Label.Text = "A fruta escolhida foi: $lista"
-$Label.Location  = New-Object System.Drawing.Point(0,10)
-$Label.AutoSize = $true
-$form.Controls.Add($Label)
+
+# adicionar imagem
+$pictureBox = New-Object Windows.Forms.PictureBox
+$pictureBox.Width = 400
+$pictureBox.Height = 400
+$pictureBox.Location = New-Object System.Drawing.Point(50, 50)
+# caminho da imagem
+$imagePath = "C:\Users\guj4ca\Downloads\teste.png"
+# carrega a imagem
+$pictureBox.Image = [System.Drawing.Image]::FromFile($imagePath)
+
+# adiciona o PictureBox à janela
+$form.Controls.Add($pictureBox)
+
+
+#botão
+$button = New-Object Windows.Forms.Button
+$button.Text = "Clique aqui"
+$button.Width = 100
+$button.Height = 30
+$button.Location = New-Object System.Drawing.Point(200, 495)
+
+# lógica quando o botão for clicado
+$button.Add_Click({
+
+    $lista = "maçã", "laranja", "melancia", "abacaxi", "mamão", "abacate", "uva" | Get-Random
+    [System.Windows.Forms.MessageBox]::Show("A fruta escolhida foi: $lista")
+})
+
+# adiciona o botão à janela Form
+$form.Controls.Add($button)
 
 
 $form.ShowDialog() #exibir tela
